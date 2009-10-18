ec8f6d1273cd658d9f5a0ff559bb94edb0de5f8a
dchichkov <dchichkov@dd86f6e2-a1c2-11de-a69d-91aafc7ba57a>
2009-10-18 01:18:11 +0000

    run.01.  in progress.

--- qrng/qrng.c
+++ qrng/qrng.c
@@ -20,7 +20,7 @@
 int main(int argc, char *argv[])
 {
 	int cardnumber = 0;
-	unsigned int bytesnum = 1024*16;
+	unsigned int bytesnum = 10000;
 	int i,j,k, cnt;
 	long long CC = 0;
 	long long C = 0;
@@ -58,16 +58,16 @@ int main(int argc, char *argv[])
 
 		for(i = 0; i < bytesnum; i++)
 		{
-			unsigned char byte = buffer[i];
+			unsigned char byte = buffer[i] ^ 0x55;
 			for(j = 0; j < 8; j++)
 			{
-				unsigned char bit = ((byte >> j) ^ j) & 0x01;
+				unsigned char bit = (byte >> j) & 0x01;
 				C++; CC += bit;
 				if(bit) for(k = 0; k < 200; k++) sqrt(42);
 			}
 		}
-
-		printf("\r%2.15Lf, %15Lf", (long double)CC/(long double)C, (long double)C);
+		if(C % (bytesnum * 100000) == 0)
+			printf("\n%2.15Lf, %15Lf", (long double)CC/(long double)C, (long double)C);
 	}
 
 	return 0;
