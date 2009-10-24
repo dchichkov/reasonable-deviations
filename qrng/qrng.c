7aaacb8c1be6c4776086532eebbcc0524e08187b
dchichkov <dchichkov@dd86f6e2-a1c2-11de-a69d-91aafc7ba57a>
2009-10-24 19:31:25 +0000

    *using MT

--- qrng/qrng.c
+++ qrng/qrng.c
@@ -10,6 +10,7 @@
 #endif
 
 #include "quantis.h"
+#include "mt19937.h"
 
 
 
@@ -18,25 +19,32 @@
 */
 int main(int argc, char *argv[])
 {
-	int cardnumber = 0;
-	unsigned int bytesnum = 1024;
-	unsigned int byte_popcount[256];
+	const int cardnumber = 0;
 	int i,j,k, cnt;
 	long long CC = 0;
 	long long C = 0;
 	long double S = 0;
 	time_t T0;	
 
+	unsigned long init[3]={6, 9, 42}, length=3;	// Init MT19937
+	const unsigned int bytesnum = 1024;			// QRNG block size
+	unsigned int byte_popcount[256];
 	unsigned char *buffer = (unsigned char *) malloc(bytesnum);
+	
 	if (buffer == NULL) {
 		fprintf(stderr, "bytes number too big!\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// Init MT19937
+    init_by_array(init, length);
+
+	// Init byte popcount
 	for(i = 0; i < 256; i++)
 	for(byte_popcount[i] = 0, k = 0; k < 8; k++)
+		if((i) & (1 << k)) byte_popcount[i]++;
 		// if((i ^ 0x55) & (1 << k)) byte_popcount[i]++;
-		if((i ^ 0xAA) & (1 << k)) byte_popcount[i]++;
+		// if((i ^ 0xAA) & (1 << k)) byte_popcount[i]++;
 
 	cnt = quantisCount();
 
@@ -66,13 +74,13 @@ int main(int argc, char *argv[])
 
 		for(i = 0; i < bytesnum; i++)
 		{
-			int K = byte_popcount[buffer[i]];
+			int K = byte_popcount[buffer[i] ^ genrand_int8()];
 			C += 8; CC += K; K <<= 7;
 			for(k = 0; k < K; k++) S += sqrt(42);
 		}
 		
 		if(C % (bytesnum * 1000000) == 0)
-			printf("\n%2.15Lf, %lld, %lld, %lld, %Lf", (long double)CC/(long double)C, C, CC, C/(time(NULL) - T0), S);
+			printf("\n%lld, %2.15Lf, %lld, %lld, %Lf", C, (long double)CC/(long double)C, CC, C/(time(NULL) - T0), S);
 	}
 
 	return 0;
