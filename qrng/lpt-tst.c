e26f266312702cbefbb9e191f3e31df1c2e5b3c2
dchichkov <dchichkov@dd86f6e2-a1c2-11de-a69d-91aafc7ba57a>
2009-12-22 06:52:27 +0000

    *internal

--- qrng/lpt-tst.c
+++ qrng/lpt-tst.c
@@ -1,54 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/ppdev.h>
-#include <linux/parport.h>
+#include <sys/io.h>
+#include <math.h>
+
+#define LPT1 0x378
+#define LPT2 0x278
 
 
 
 int main(int argc, char *argv[])
 {
-	unsigned char buf[1000000];
-	time_t T0 = time(NULL);
+  long long i;
+  time_t T0 = time(NULL);
+  long long k;
+  long double S = 0.0;
 
-	int fd, mode;
-	int buff[1];
-	if ((fd=open("/dev/parport0", O_RDWR))<0)
-	{
-		printf("\nnu deschide\n");
-		exit(0);
-	}
-	
-	if (ioctl(fd, PPCLAIM)<0)
-	{
-		printf("\nnu mi-l claimuieste\n");
-		perror("PPCLAIM");
-		close(fd);
-		exit(0);
-	}
-	
-	mode = IEEE1284_MODE_ECP;
-	if (ioctl(fd, PPSETMODE, &mode)<0)
-	{
-		printf("\nnu seteaza modu ecp\n");
-		perror("PPSETMODE");
-		close(fd);
-		exit(0);
-	}
-	
-	printf("\nwrite\n");
-	write(fd,"123",3);
-	sleep(1);
-	buff[0]=0;
-	printf("\nwrite\n");
-	write(fd, buff,1);
-	
-	if (ioctl(fd, PPRELEASE)<0)
-	{
-		printf("\nnu face release\n");
-		close(fd);
-	} 
-   printf("\n T = %d\n", (int)(time(NULL) - T0));
+  ioperm(LPT1, 1, 1);   // 1 port, true
+  for(i = 0; i < 10000000000; i++)
+  {
+          outb(0x0F, LPT1);
+	  outb(0x0F, LPT1);	  
+	  outb(0x00, LPT1);
+	  outb(0x00, LPT1);
+
+  }
+
+   printf("\n%d\n", (int)(time(NULL) - T0));
+
+
 }
+
